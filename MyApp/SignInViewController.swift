8963d5d3eb3ca23865a256dd1f6477819092ad69
EliaJade <eliajadesmith23@gmail.com>
2025-06-03 13:39:47 +0200

    sign in, sign up with google works, sign up WIP

--- MyApp/SignInViewController.swift
+++ MyApp/SignInViewController.swift
@@ -7,9 +7,11 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseCore
+import GoogleSignIn
 
 class SignInViewController: UIViewController {
-
+    
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var usernameTextField: UITextField!
     
@@ -17,28 +19,36 @@ class SignInViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func signUp(_ sender: Any) {
         let username = usernameTextField.text ?? ""
         let password = passwordTextField.text ?? ""
         
         Auth.auth().createUser(withEmail: username, password: password) { authResult, error in
-            /*if let error = error {
-            // Hubo un error
-            print(error)
+            if let error = error {
+                // Hubo un error
+                print(error)
                 
-            let alertController = UIAlertController(title: "Create user", message: error.localizedDescription, prefe
-                    alertController.addAction(UIAlertAction(title: "OK", style: .default))
-            self-present(alertController, animated: true, completion: nil)
+                let alertController = UIAlertController(title: "Create user", message: error.localizedDescription, preferredStyle: .alert)
+                
+                alertController.addAction(UIAlertAction(title: "OK", style: .default))
+                
+                self.present(alertController, animated: true, completion: nil)
+                
+            } else {
+                // Todo correcto
+                print("User signs up successfully")
+                
+                let alertController = UIAlertController(title: "Create user", message: "Signed up succesfully", preferredStyle: .alert)
+                
+                alertController.addAction(UIAlertAction(title: "OK", style: .default))
+                
+                self.present(alertController, animated: true, completion: nil)
             }
-            else {
-            // Todo correcto
-            print("User signs
-            up successfully")*/
         }
     }
-                
-            
+    
+    
     
     @IBAction func signIn(_ sender: Any) {
         let username = usernameTextField.text ?? ""
@@ -46,20 +56,64 @@ class SignInViewController: UIViewController {
         
         Auth.auth().signIn(withEmail: username, password: password) { [unowned self] authResult, error in
             if let error = error {
+                // Hubo un error
+                print (error)
+                
+                let alertController = UIAlertController(title: "Sign In", message: error.localizedDescription, preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "OK", style: .default))
+                self.present (alertController, animated: true, completion: nil)
+            } else {
+                print("User signs in successfully")
+                goToHome()
+                
+            }
+            
+        }
+    }
+    
+    @IBAction func signInGoogle(_ sender: Any) {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+        
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            
+            guard let user = result?.user, let idToken = user.idToken?.tokenString else {
+                return
+            }
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
+            
+            Auth.auth().signIn(with: credential) { result, error in
+                
+                if let error = error {
                     // Hubo un error
                     print (error)
-                
-                    let alertController = UIAlertController(title: "Sign In", message:
-                        error.localizedDescription, preferredStyle: .alert)
+                    
+                    let alertController = UIAlertController(title: "Sign In", message: error.localizedDescription, preferredStyle: .alert)
                     alertController.addAction(UIAlertAction(title: "OK", style: .default))
                     self.present (alertController, animated: true, completion: nil)
                 } else {
                     print("User signs in successfully")
                     goToHome()
                     
+                    // At this point, our user is signed in
                 }
-
+                
+                // ...
+            }
         }
     }
-}
+        func goToHome() {
+            self.performSegue(withIdentifier: "goToHome", sender: nil)
+        }
+    }
+    
 
