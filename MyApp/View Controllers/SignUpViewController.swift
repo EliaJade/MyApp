6f517ec1569ea980e6e16b25fd221591cdcb35de
EliaJade <eliajadesmith23@gmail.com>
2025-06-05 13:38:53 +0200

    sign up done

---
+++ MyApp/View Controllers/SignUpViewController.swift
@@ -0,0 +1,124 @@
+//
+//  SignUpViewController.swift
+//  MyApp
+//
+//  Created by Mañanas on 5/6/25.
+//
+
+import UIKit
+import FirebaseAuth
+
+class SignUpViewController: UIViewController {
+    
+    //MARK: Outlets
+
+    @IBOutlet weak var firstnamesTextField: UITextField!
+    @IBOutlet weak var surnamesTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    
+    @IBOutlet weak var genderSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var genderImageView: UIImageView!
+    
+    @IBOutlet weak var dateOfBirthPicker: UIDatePicker!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let maxDate = Calendar.current.date(byAdding: .year, value: -18, to: Date())
+        dateOfBirthPicker.maximumDate = maxDate
+        // Do any additional setup after loading the view.
+    }
+    
+    func validate() -> Bool {
+        if firstnamesTextField.text?.isEmpty ?? true {
+            showValidationError(error: "First names cannot be empty")
+            return false
+        }
+        
+        if surnamesTextField.text?.isEmpty ?? true {
+            showValidationError(error: "Surnames cannot be empty")
+            return false
+        }
+        
+        if emailTextField.text?.isEmpty ?? true {
+            showValidationError(error: "Email cannot be empty")
+            return false
+        }
+        
+        if passwordTextField.text?.isEmpty ?? true {
+            showValidationError(error: "Password cannot be empty")
+            return false
+        }
+        
+        if passwordTextField.text!.count < 6 {
+            showValidationError(error: "Password must have more than 6 characters")
+            return false
+        }
+        
+        if repeatPasswordTextField.text?.isEmpty ?? true {
+            showValidationError(error: "You must repeat the password to confirm it")
+            return false
+        }
+        
+        if passwordTextField.text != repeatPasswordTextField.text {
+            showValidationError(error: "Password does not match")
+            return false
+        }
+        return true
+    }
+    
+    func showValidationError (error: String) {
+        let alert = UIAlertController(title: nil, message: error, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func signUp(_ sender: Any) {
+        if validate() {
+            let username = emailTextField.text ?? ""
+            let password = passwordTextField.text ?? ""
+            
+            Auth.auth().createUser(withEmail: username, password: password) { authResult, error in
+                if let error = error {
+                    // Hubo un error
+                    print(error)
+                    
+                    let alertController = UIAlertController(title: "Create user", message: error.localizedDescription, preferredStyle: .alert)
+                    
+                    alertController.addAction(UIAlertAction(title: "OK", style: .default))
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                    
+                } else {
+                    // Todo correcto
+                    print("User signs up successfully")
+                    
+                    let alertController = UIAlertController(title: "Create user", message: "Signed up succesfully", preferredStyle: .alert)
+                    
+                    alertController.addAction(UIAlertAction(title: "OK", style: .default))
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func genderSegmentedControl(_ sender: Any) {
+        switch genderSegmentedControl.selectedSegmentIndex {
+        case 0:
+            genderImageView.image = UIImage(named: "genderIcon-female")
+        case 1:
+            genderImageView.image = UIImage(named: "genderIcon-male")
+        default:
+            genderImageView.image = UIImage(named: "genderIcon-other")
+            break
+        }
+    }
+    
+
+}
